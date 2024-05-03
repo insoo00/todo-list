07045ae5011b814cce8369d04aee62b60ee90513
insoo00 <insoosos1234@gmail.com>
2024-05-03 17:14:52 +0900

    :tada: initial commit

---
+++ toy/src/test/java/spring/toy/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.toy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

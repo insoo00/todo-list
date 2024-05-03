61d51bbe91dd14733dfe065585e34365298d2620
insoo00 <insoosos1234@gmail.com>
2024-05-03 23:53:33 +0900

    :memo: Add SocialClientRegistration

--- toy/src/main/java/spring/toy/oauth2/SocialClientRegistration.java
+++ toy/src/main/java/spring/toy/oauth2/SocialClientRegistration.java
@@ -7,4 +7,56 @@ import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SocialClientRegistration {}
+public class SocialClientRegistration {
+
+    public ClientRegistration naverClientRegistration() {
+
+        return ClientRegistration
+                .withRegistrationId("naver")
+                .clientId("K2VUk0fHtxPi2uajiiGT")
+                .clientSecret("NUjcEM7ElB")
+                .redirectUri("http://localhost:8080/login/oauth2/code/naver")
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .scope("name", "email")
+                .authorizationUri("https://nid.naver.com/oauth2.0/authorize")
+                .tokenUri("https://nid.naver.com/oauth2.0/token")
+                .userInfoUri("https://openapi.naver.com/v1/nid/me")
+                .userNameAttributeName("response")
+                .build();
+    }
+
+    public ClientRegistration googleClientRegistration() {
+
+        return ClientRegistration
+                .withRegistrationId("google")
+                .clientId("717679495774-ipegpu1fiaa7ajqru25ggp7ve4m4se1h.apps.googleusercontent.com")
+                .clientSecret("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")
+                .redirectUri("http://localhost:8080/login/oauth2/code/google")
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .scope("profile", "email")
+                .authorizationUri("https://accounts.google.com/o/oauth2/v2/auth")
+                .tokenUri("https://www.googleapis.com/oauth2/v4/token")
+                .jwkSetUri("https://www.googleapis.com/oauth2/v3/certs")
+                .issuerUri("https://accounts.google.com")
+                .userInfoUri("https://www.googleapis.com/oauth2/v3/userinfo")
+                .userNameAttributeName(IdTokenClaimNames.SUB)
+                .build();
+    }
+
+    public ClientRegistration kakaoClientRegistration() {
+
+        return ClientRegistration
+                .withRegistrationId("kakao")
+                .clientId("5d6254d204d540d6941db038d4e6da3f")
+                .clientSecret("fqg9dWugZPlb5rdLRwZmokhgvsaMs1oi")
+                .redirectUri("http://localhost:8080/login/oauth2/code/kakao")
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_POST)
+                .scope("profile_nickname")
+                .authorizationUri("https://kauth.kakao.com/oauth/authorize")
+                .tokenUri("https://kauth.kakao.com/oauth/token")
+                .userInfoUri("https://kapi.kakao.com/v2/user/me")
+                .userNameAttributeName("kakao_account")
+                .build();
+    }
+}
